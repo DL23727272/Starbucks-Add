0d63c15c2226d9e18a0f1f26e89b4b9a67ceb29e
DL23727272 <113783762+DL23727272@users.noreply.github.com>
2024-03-26 10:04:22 +0800

    updated adminpage adjusted the position using bootstrap and their margin to be responsive

--- adminFetchProduct.php
+++ adminFetchProduct.php
@@ -7,10 +7,10 @@
 
     $productCount = 0;
 
-    echo '<div class="row">';
+    echo '<div class="row justify-content-center m-2">';
 
     while ($row = mysqli_fetch_assoc($result)) {
-        echo '<div class="card m-4 p-3 col-12 col-md-6 col-lg-3">';
+        echo '<div class="card mt-4 m-3 p-3 col-12 col-md-6 col-lg-3">';
         echo '<div class="d-flex justify-content-center">';
         echo '<img src="products/'. $row['productImage'] .'" alt="Frontrow" style="max-width: 100%; height: 200px; object-fit: cover; filter: drop-shadow(5px 5px 15px #006341)"/>';
         echo '</div>';
@@ -28,7 +28,7 @@
         // Check if 4 products have been printed (to start a new row)
         if ($productCount % 3 == 0) {
             echo '</div>'; // Close the current row
-            echo '<div class="row">'; // Start a new row
+            echo '<div class="row justify-content-center m-2">'; // Start a new row
         }
     }
 
