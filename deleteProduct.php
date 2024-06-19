f767968cddf5c9cfe870f945ed58e7e754ad15fd
DL23727272 <113783762+DL23727272@users.noreply.github.com>
2024-06-19 12:51:26 +0800

    fixed the delete product function

--- deleteProduct.php
+++ deleteProduct.php
@@ -1,5 +1,5 @@
 <?php
-include 'myConnection.php';
+/*include 'myConnection.php';
 
 if(isset($_POST['id'])) {
     $productID = $_POST['id'];
@@ -13,5 +13,29 @@ if(isset($_POST['id'])) {
     }
 } else {
     echo "Product ID not received"; 
+}*/
+
+
+//updated to delete product
+include 'myConnection.php';
+
+if(isset($_POST['id'])) {
+    $productID = $_POST['id'];
+    
+    $sql = "DELETE FROM order_items_table WHERE productID = $productID";
+    if (!mysqli_query($con, $sql)) {
+        echo 'Error: ' . mysqli_error($con);
+        exit;
+    }
+    
+    $sql = "DELETE FROM product_table WHERE productID = $productID";
+    if (mysqli_query($con, $sql)) {
+        echo 'Success';
+    } else {
+        echo 'Error: ' . mysqli_error($con);
+    }
+} else {
+    echo "Product ID not received"; 
 }
+
 ?>
